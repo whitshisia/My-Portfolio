3b034b9adbfb5e73b2bb277644b4f0b96eb393e7
Whitney Shisia <shisiawhitney215@gmail.com>
2025-03-19 17:53:37 +0300

    redeployment

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  dir: './src', // This tells Next.js to look for pages/app in the `src` folder
+
 };
 
 export default nextConfig;
